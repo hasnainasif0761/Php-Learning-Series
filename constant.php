487ca90a469fc60ceef5b731d5376a5f2e26a721
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-01-29 12:30:34 +0500

    3-Updation

---
+++ constant.php
@@ -0,0 +1,27 @@
+<!-- <//?php
+
+// const data = "Personal Info";
+// // const data = "Hello Hey👋";
+
+// echo data;
+
+// define('data','Huzaifa');
+
+// echo data;
+
+
+// const __ = "Hello Asif bhai";  // Declare the const for __ 
+
+// echo __ ;
+
+
+// //?//>
+
+//<//?php 
+
+// $name = "Tony";
+// $name = "Raja";
+
+// echo $name;
+
+?>
