aa5ec825b7c94e30a55b55e91623fe1928dd5f25
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-01-29 12:17:04 +0500

    'first-Updation'

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = "Huzaifa";
+$color= '#000';
+
+echo "<h1 style='color:red;'>My name is $name</h1>";
+
+?>
+
+
+<h2 style="color:<?php echo $color   ?>;">Hello My name is <?php echo $name ?></h2>
