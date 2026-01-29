a34adf48c785c651362a2cce1b788401fbdf0da8
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-01-29 12:46:09 +0500

    4-Updation

---
+++ datatype.php
@@ -0,0 +1,38 @@
+<?php
+// $name = "Anil Sidhu";
+
+// echo var_dump($name);
+
+// $num = 10;
+
+// echo $num;
+// echo var_dump($num);
+
+
+// $numFloat  = 10.10;
+
+// echo var_dump($numFloat);
+
+// $blooe = true;
+
+// echo var_dump($blooe);
+
+
+
+// $arr = ['hello','how','are ',10];
+
+// echo var_dump($arr);
+
+
+// $empty = null;
+
+// echo var_dump($empty);
+
+
+
+// $user = new User();
+
+
+
+
+?>
